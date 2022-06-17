8c96a90a0138ac9785e6f43c930b1b6c99a16735
Alberto <alberto.presenti@eng.it>
2022-06-17 16:09:27 +0200

    web3modal init configuration

--- src/app/core/services/wallet.service.ts
+++ src/app/core/services/wallet.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import Web3 from "web3";
+import Web3Modal from "web3modal";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WalletService {
 
-  constructor() { }
+  private web3js: any;
+  private provider: any;
+  private accounts: any;
+  private web3Modal;
+
+  private accountStatusSource = new Subject<any>();
+  accountStatus$ = this.accountStatusSource.asObservable();
+
+  constructor() {
+
+
+  }
 
   async connect() {
     if (window.ethereum) {
