d794a577d74d9cd9997c3a2b39160890974a4a72
Albenzoo <alberto.presenti@yahoo.it>
2022-07-17 13:40:50 +0200

    Show wallet address when connected

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -4,17 +4,14 @@ import { WalletService } from 'src/app/core/services/wallet.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(public wallet: WalletService) {}
 
-  constructor(private wallet: WalletService) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   connectWallet() {
     this.wallet.connect();
   }
-
 }
