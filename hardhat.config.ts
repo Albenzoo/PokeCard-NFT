a62d2b04818555ea118e8ef773db53764d16f300
Alberto <alberto.presenti@eng.it>
2022-08-30 17:32:43 +0200

    Setup and compile hardhat contract

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig } from "hardhat/config";
+/* import "@nomicfoundation/hardhat-toolbox"; */
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const { API_URL, PRIVATE_KEY } = process.env;
+console.log({ PRIVATE_KEY, API_URL });
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+};
+
+export default config;
