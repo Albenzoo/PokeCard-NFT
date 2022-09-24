edb49ba8fbf29ab87d8f3b21454b150f74264edf
Albenzoo <alberto.presenti@yahoo.it>
2022-09-24 20:38:58 +0200

    experimenting metadata retrieve

--- src/app/core/services/wallet.service.ts
+++ src/app/core/services/wallet.service.ts
@@ -4,17 +4,18 @@ import * as contract from '../../../../artifacts/contracts/MyNFT.sol/MyNFT.json'
 import { AbiItem } from 'web3-utils';
 import { ContractInfo } from '../models/contract-info';
 import { resolve } from 'dns';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WalletService {
-  constructor() { }
+  constructor() {}
 
   web3: any = new Web3(window.ethereum);
   walletAddress: string = '';
   contractInfo: ContractInfo = {
-    contractAddress: '0x99151d26159362457c41b8c96a93680ef776b7dd',
+    contractAddress: environment.contractAddress,
     contractABI: contract.abi as AbiItem[],
   };
   nftContract = new this.web3.eth.Contract(
@@ -25,54 +26,37 @@ export class WalletService {
   public getNftBalance(): Promise<number> {
     return new Promise((resolve, reject) => {
       this.nftContract.defaultAccount = this.walletAddress;
-      this.nftContract.methods.balanceOf(this.walletAddress).call(function (err: any, result: any) {
-        if (err != null) {
-          reject(err);
-        }
-        console.log("Numero di NFT: ", result);
-        resolve(result);
-      });
+      this.nftContract.methods
+        .balanceOf(this.walletAddress)
+        .call(function (err: any, result: any) {
+          if (err != null) {
+            reject(err);
+          }
+          console.log('Numero di NFT: ', result);
+          resolve(result);
+        });
     }) as Promise<number>;
   }
 
-  public getTokenIdfirst() {
-    const tokenId = new Promise((resolve, reject) => {
-      this.nftContract.defaultAccount = this.walletAddress;
-      resolve(this.nftContract.methods.tokenOfOwnerByIndex(this.walletAddress, 0));
-
-    }) as Promise<any>;
-    console.log({ tokenId });
-
-    const tokemMetadataURI = new Promise((resolve, reject) => {
-      resolve(this.nftContract.methods.tokenURI(tokenId).call());
-    }) as Promise<any>;
-    console.log({ tokemMetadataURI });
-
-  }
-  public getTokenId2() {
-    const tokenId = new Promise((resolve, reject) => {
-      this.nftContract.defaultAccount = this.walletAddress;
-      return this.nftContract.methods.tokenOfOwnerByIndex(this.walletAddress, 0);
-
-    });
-    console.log({ tokenId });
-
-    const tokemMetadataURI = new Promise((resolve, reject) => {
-      resolve(this.nftContract.methods.tokenURI(tokenId).call());
-    });
-    console.log({ tokemMetadataURI });
-
-  }
   public getTokenId() {
     this.nftContract.defaultAccount = this.walletAddress;
-
-    const tokenId = Promise.resolve(this.nftContract.methods.tokenOfOwnerByIndex(this.walletAddress, 5));
-    tokenId.then((res) => {
-      const tokemMetadataURI = this.nftContract.methods.tokenURI(tokenId).call();
-    })
-
+    let tokenId;
+    let tokemMetadataURI;
+    this.nftContract.methods
+      .tokenOfOwnerByIndex(this.walletAddress, 0)
+      .call()
+      .then((res: any) => {
+        tokenId = res;
+        console.log({ tokenId });
+        this.nftContract.methods
+          .tokenURI(tokenId)
+          .call()
+          .then((res: any) => {
+            tokemMetadataURI = res;
+            console.log({ tokemMetadataURI });
+          });
+      });
   }
-
 
   connect(): boolean {
     if (window.ethereum) {
