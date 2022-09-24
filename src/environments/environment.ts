c11f7231ae86c48bcdf4bf7255a8988b3831b485
Albenzoo <alberto.presenti@yahoo.it>
2022-09-24 20:38:12 +0200

    update environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,6 @@
 export const environment = {
-    production: false,
+  production: false,
+  contractAddress: '0x6e9ff2411e1d02e5cc866e6c824a52d2920202e6',
+  apiUrl:
+    'https://eth-goerli.g.alchemy.com/v2/201UMLnLvF_3ShqOfxSOX_-jiXKbRe7H',
 };
