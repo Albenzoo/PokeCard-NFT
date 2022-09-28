6b4ea2d8a72c0ba7141887077a25d58eb1ee8d99
Alberto <alberto.presenti@eng.it>
2022-09-28 17:09:35 +0200

    changed contract factory name

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,7 +2,7 @@ import { ethers } from 'hardhat';
 
 async function main() {
   console.log({ ethers });
-  const MyNFT = await ethers.getContractFactory("MyNFT");
+  const MyNFT = await ethers.getContractFactory("NFTCardMarketplace");
 
   // Start deployment, returning a promise that resolves to a contract object
   const myNFT = await MyNFT.deploy();
