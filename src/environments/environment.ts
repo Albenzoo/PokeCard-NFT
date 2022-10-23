8f46849beac27b153f366c62cb387c4f7634a031
Albenzoo <alberto.presenti@yahoo.it>
2022-10-23 22:07:37 +0200

    general minor update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  contractAddress: '0x6e9ff2411e1d02e5cc866e6c824a52d2920202e6',
+  contractAddress: '0x93948a0e51E556728aF0862Ab258C0aBCD2BdDc1',
   apiUrl:
     'https://eth-goerli.g.alchemy.com/v2/201UMLnLvF_3ShqOfxSOX_-jiXKbRe7H',
 };
