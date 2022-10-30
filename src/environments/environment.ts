8434a89dd43e9861882eebc57d60eb513886aa1c
Alberto <alberto.presenti@eng.it>
2022-10-30 18:52:28 +0100

    completed api call to get all lnfts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,12 @@ export const environment = {
   contractAddress: '0x93948a0e51E556728aF0862Ab258C0aBCD2BdDc1',
   apiUrl:
     'https://eth-goerli.g.alchemy.com/v2/201UMLnLvF_3ShqOfxSOX_-jiXKbRe7H',
-  pinataBaseUrl: 'https://gateway.pinata.cloud/',
-  ipfsBaseUrlGeteway: ['https://hardbin.com/',
+  ipfsBaseUrl: 'https://ipfs.io/',
+  ipfsBaseUrlGeteway: [
+    'https://ipfs.io/',
+    'https://hardbin.com/',
     'https://cloudflare-ipfs.com/',
     'https://gateway.ipfs.io/',
-    'https://ipfs.io/',
+
     'https://gateway.pinata.cloud/']
 };
