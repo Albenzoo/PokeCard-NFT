e160e3153850397da1797f6ea8e09823e73af62f
Alberto <alberto.presenti@eng.it>
2022-12-03 15:51:49 +0100

    test with image input

--- src/app/pages/create-nft/create-nft.component.ts
+++ src/app/pages/create-nft/create-nft.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { AsyncSubject, BehaviorSubject, connect, connectable, filter, fromEvent, interval, map, Observable, ReplaySubject, scan, share, Subject, tap } from 'rxjs';
+import { webSocket } from 'rxjs/webSocket';
+import { ajax } from "rxjs/ajax";
+import { ApiService } from 'src/app/core/services/api.service';
 
+
+
+interface UserResponse {
+  data: {
+    id: number;
+    email: string;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+  };
+}
 @Component({
   selector: 'app-create-nft',
   templateUrl: './create-nft.component.html',
   styleUrls: ['./create-nft.component.scss']
 })
-export class CreateNftComponent implements OnInit {
 
-  constructor() { }
+export class CreateNftComponent implements OnInit {
+  imageFile: File = <File>{};
+
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+
+
+  }
+
+
+  onFileSelected(event: any) {
+    const file: File = event?.target?.files[0];
+    if (file) {
+      this.imageFile = file;
+
+
+      //const formData = new FormData();
+      //formData.append("thumbnail", file);
+      //const upload$ = this.http.post("/api/thumbnail-upload", formData);
+      //upload$.subscribe();
+    }
+  }
+  uploadCard() {
+    this.apiService.loadCardToIPFS(this.imageFile);
+
   }
 
 }
