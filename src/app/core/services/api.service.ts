294d92368e346f84d18e76d1355ee96c5cd6fffe
Alberto <alberto.presenti@eng.it>
2022-12-03 17:27:30 +0100

    Completed NFT creation logic

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,29 +1,19 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, concat, concatMap, map, Observable, of, switchMap } from 'rxjs';
+import { catchError, concat, concatMap, from, map, Observable, of, switchMap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Card } from '../models/card';
 import { PinataService } from './pinata.service';
+import { WalletService } from './wallet.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient, private pinataService: PinataService) { }
+  constructor(private http: HttpClient, private pinataService: PinataService, public walletService: WalletService) { }
 
-  getNftInfo(url: string): Observable<any> {
-    return this.http
-      .get(url).pipe(map((response: any) => {
-        // modify the response  data
-        if (response.image) {
-          response.image = response.image.replace('ipfs://', environment.ipfsBaseUrl + 'ipfs/');
-        }
-        // return the modified data
-        return response;
-      })
-        ,);
-  }
+
 
 
   loadCardToIPFS(image: any) {
@@ -59,69 +49,22 @@ export class ApiService {
     };
 
     this.pinataService.uploadFileToIPFS(image).pipe(
-      switchMap(response => {
-        console.log({ response });
-        card.image = response.pinataURL;
+      switchMap(response1 => {
+        console.log("Card image response:", response1);
+        card.image = response1.pinataURL;
         return this.pinataService.uploadJSONToIPFS(card);
-      })
+      }),
+      switchMap(response2 => from(this.walletService.mintNFT(response2.pinataURL)))
     ).subscribe({
       next: (data: any) => {
-        console.log("successo:", { data });
+        console.log("Finale response:", data);
       },
       error: (error: any) => {
         console.log("error uploading the card:", error);
       },
     });
 
-
-
-
   }
 
-
-  /* uploadMetadataToIPFS() {
-    let card: Card = {
-      name: "Charmander",
-      hp: 50,
-      //image: 'image',
-      length: 2.0,
-      weight: 19,
-      type: 'Lizard',
-      energy_type: 'Fire',
-      rarity: 'Common',
-      attack_list: [
-        {
-          cost: ["Colorless"],
-          name: "Scratch",
-          damage: 10
-        },
-        {
-          cost: ["Fire", "Colorless"],
-          name: "Ember",
-          text: "Discard I Fire Energy card attached to Charmander in order to use this attack.",
-          damage: 30
-        }
-      ],
-      description: 'Obviously prefers hot places. If it gets caught in the rain, steam is said to spout from the tip of his tail.',
-      level: 10,
-      weaknesses: ["Water"],
-      resistance: [],
-      retreatCost: ["Colorless"],
-      artist: 'Mitsuhiro Arita',
-      number: '46/102'
-    };
-
-
-    //upload the metadata JSON to IPFS
-    this.pinataService.uploadJSONToIPFS(card).subscribe({
-      next: (data: any) => {
-        console.log({ data });
-      },
-      error: (error: any) => {
-        console.log("error uploading JSON metadata:", error);
-      },
-    });
-
-  } */
 
 }
