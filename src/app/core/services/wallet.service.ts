426b98dd345b9a8245fd017d18945b98ed27a8ba
Alberto <alberto.presenti@eng.it>
2022-12-03 17:27:30 +0100

    Completed NFT creation logic

--- src/app/core/services/wallet.service.ts
+++ src/app/core/services/wallet.service.ts
@@ -8,21 +8,26 @@ import { environment } from 'src/environments/environment';
 import { any, json } from 'hardhat/internal/core/params/argumentTypes';
 import { ApiService } from './api.service';
 import { url } from 'inspector';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WalletService {
-  constructor(private apiService: ApiService) { }
+  constructor(private http: HttpClient) { }
 
-  web3: any = new Web3(window.ethereum);
+  web3Instance: any = new Web3(window.ethereum);
+  web3 = createAlchemyWeb3(
+    environment.apiUrl,
+  );
   walletAddress: string = '';
   contractInfo: ContractInfo = {
     contractAddress: environment.contractAddress,
     contractABI: contract.abi as AbiItem[],
   };
-  nftContract = new this.web3.eth.Contract(
+  nftContract = new this.web3Instance.eth.Contract(
     this.contractInfo.contractABI,
     this.contractInfo.contractAddress
   );
@@ -44,10 +49,21 @@ export class WalletService {
     }) as Promise<number>;
   }
 
-
+  getNftInfo(url: string): Observable<any> {
+    return this.http
+      .get(url).pipe(map((response: any) => {
+        // modify the response  data
+        if (response.image) {
+          response.image = response.image.replace('ipfs://', environment.ipfsBaseUrl + 'ipfs/');
+        }
+        // return the modified data
+        return response;
+      })
+        ,);
+  }
   storeDataFromURI(url: string): void {
     url = url.replace(':/', '');
-    this.apiService.getNftInfo(environment.ipfsBaseUrl + url).subscribe({
+    this.getNftInfo(environment.ipfsBaseUrl + url).subscribe({
       next: (data: any) => {
         console.log({ data });
         this.allNfts.push(data);
@@ -73,6 +89,57 @@ export class WalletService {
 
   }
 
+  public async mintNFT(tokenURI: string) {
+    const nonce = await this.web3.eth.getTransactionCount(environment.PUBLIC_KEY, 'latest'); //get latest nonce
+
+    const inputPrice: number = 0.02;
+    const price = this.web3.utils.toWei(inputPrice.toString(), 'ether');
+    const priceBN = this.web3.utils.toBN(price);
+    console.log('NFT price set:', price, 'Wei');
+
+    //get the fee price to be payed in every transaction
+    let feePrice = await this.nftContract.methods.getListPrice().call();
+    feePrice = feePrice.toString();
+    console.log('Fee:', feePrice, 'Wei');
+
+    //actually create the NFT
+    //return;
+    //the transaction
+    const tx = {
+      from: environment.PUBLIC_KEY,
+      to: environment.contractAddress,
+      nonce: nonce,
+      gas: 500000,
+      data: this.nftContract.methods.createToken(tokenURI, priceBN).encodeABI(),
+      value: feePrice,
+    };
+
+    const signPromise = this.web3.eth.accounts
+      .signTransaction(tx, environment.PRIVATE_KEY)
+      .then((signedTx: any) => {
+        this.web3.eth.sendSignedTransaction(
+          signedTx.rawTransaction,
+          function (err, hash) {
+            if (!err) {
+              console.log(
+                'The hash of your transaction is: ',
+                hash,
+                "\nCheck Alchemy's Mempool to view the status of your transaction!"
+              );
+            } else {
+              console.log(
+                'Something went wrong when submitting your transaction:',
+                err
+              );
+            }
+          }
+        );
+      })
+      .catch((err) => {
+        console.log(' Promise failed:', err);
+      });
+  }
+
   connect(): boolean {
     if (window.ethereum) {
       console.log('Metamask is installed!');
@@ -83,10 +150,10 @@ export class WalletService {
         .then((accountsAddress: string) => {
           this.walletAddress = accountsAddress[0];
           console.log(this.walletAddress);
-          this.web3 = new Web3(window.ethereum);
+          this.web3Instance = new Web3(window.ethereum);
           //const accounts = this.web3Instance.eth.getAccounts();
 
-          console.log('Account connected successfully!', this.web3);
+          console.log('Account connected successfully!', this.web3Instance);
           return true;
         })
         .catch(() => {
--- src/app/pages/create-nft/create-nft.component.ts
+++ src/app/pages/create-nft/create-nft.component.ts
@@ -29,7 +29,6 @@ export class CreateNftComponent implements OnInit {
 
   ngOnInit() {
 
-
   }
 
 
