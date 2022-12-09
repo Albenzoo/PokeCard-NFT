82f9ed3094e3b863134de3792a43b6394e8036e6
Alberto <alberto.presenti@eng.it>
2022-12-09 17:34:41 +0100

    Improvement on creation form

--- src/app/pages/create-nft/create-nft.component.ts
+++ src/app/pages/create-nft/create-nft.component.ts
@@ -3,18 +3,12 @@ import { AsyncSubject, BehaviorSubject, connect, connectable, filter, fromEvent,
 import { webSocket } from 'rxjs/webSocket';
 import { ajax } from "rxjs/ajax";
 import { ApiService } from 'src/app/core/services/api.service';
+import { Card, Attack } from 'src/app/core/models/card';
+import { AnyForUntypedForms } from '@angular/forms';
 
 
 
-interface UserResponse {
-  data: {
-    id: number;
-    email: string;
-    first_name: string;
-    last_name: string;
-    avatar: string;
-  };
-}
+
 @Component({
   selector: 'app-create-nft',
   templateUrl: './create-nft.component.html',
@@ -23,29 +17,83 @@ interface UserResponse {
 
 export class CreateNftComponent implements OnInit {
   imageFile: File = <File>{};
+  customCard: Card = <Card>{};
+  attackList: Attack[] = [{ cost: ["Fire"] }];
+  imageLabel: string = "Load card image ->";
+  imagePreview: any;
+
+  eventsImageLoaded: Subject<void> = new Subject<void>();
 
 
-  constructor(private apiService: ApiService) { }
 
-  ngOnInit() {
+
+  constructor(private apiService: ApiService) {
 
   }
 
+  ngOnInit() {
+
+    /* this.customCard = {
+      name: "Charmander",
+      hp: 50,
+      image: "",
+      length: 2.0,
+      weight: 19,
+      type: 'Lizard',
+      energy_type: 'Fire',
+      rarity: 'Common',
+      attack_list: [
+        {
+          cost: ["Colorless"],
+          name: "Scratch",
+          text: "Discard I Fire Energy card attached to Charmander in order to use this attack.",
+
+          damage: 50
+        },
+        {
+          cost: ["Fire", "Colorless"],
+          name: "Ember",
+          text: "Discard I Fire Energy card attached to Charmander in order to use this attack.",
+          damage: 30
+        }
+      ],
+      description: 'Obviously prefers hot places. If it gets caught in the rain, steam is said to spout from the tip of his tail.',
+      level: 10,
+      weaknesses: ["Water"],
+      resistance: [],
+      retreatCost: ["Colorless"],
+      artist: 'Mitsuhiro Arita',
+      number: '46/102'
+    }; */
+
+  }
+  emitEventImageLoadedToCard(myImage: any) {
+    this.eventsImageLoaded.next(myImage);
+  }
 
   onFileSelected(event: any) {
     const file: File = event?.target?.files[0];
     if (file) {
-      this.imageFile = file;
+      if (file.type.match(/image\/*/) == null) {
+        console.warn("Only images are supported");
+        return;
+      }
 
-
-      //const formData = new FormData();
-      //formData.append("thumbnail", file);
-      //const upload$ = this.http.post("/api/thumbnail-upload", formData);
-      //upload$.subscribe();
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (event: any) => this.imagePreview = event.target.result;
+      this.emitEventImageLoadedToCard(file)
+      this.customCard.image = file;
+      this.imageLabel = this.customCard.image.name;
     }
   }
   uploadCard() {
-    this.apiService.loadCardToIPFS(this.imageFile);
+    this.customCard.attack_list = this.attackList;
+    this.customCard.hp = Number(this.customCard.hp);
+    this.customCard.level = Number(this.customCard.level);
+    console.log("customCard:", this.customCard);
+
+    this.apiService.loadCardToIPFS(this.customCard);
 
   }
 
