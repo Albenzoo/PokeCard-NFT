cf69a533dfc4dc56fe507b192cb3bfff24d257d7
Alberto <alberto.presenti@eng.it>
2022-12-09 17:35:31 +0100

    Update api call

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -16,43 +16,43 @@ export class ApiService {
 
 
 
-  loadCardToIPFS(image: any) {
-    let card: Card = {
-      name: "Charmander",
-      hp: 50,
-      image: image,
-      length: 2.0,
-      weight: 19,
-      type: 'Lizard',
-      energy_type: 'Fire',
-      rarity: 'Common',
-      attack_list: [
-        {
-          cost: ["Colorless"],
-          name: "Scratch",
-          damage: 10
-        },
-        {
-          cost: ["Fire", "Colorless"],
-          name: "Ember",
-          text: "Discard I Fire Energy card attached to Charmander in order to use this attack.",
-          damage: 30
-        }
-      ],
-      description: 'Obviously prefers hot places. If it gets caught in the rain, steam is said to spout from the tip of his tail.',
-      level: 10,
-      weaknesses: ["Water"],
-      resistance: [],
-      retreatCost: ["Colorless"],
-      artist: 'Mitsuhiro Arita',
-      number: '46/102'
-    };
+  loadCardToIPFS(newCard: Card) {
+    /*   let card: Card = {
+        name: "Charmander",
+        hp: 50,
+        image: image,
+        length: 2.0,
+        weight: 19,
+        type: 'Lizard',
+        energy_type: 'Fire',
+        rarity: 'Common',
+        attack_list: [
+          {
+            cost: ["Colorless"],
+            name: "Scratch",
+            damage: 10
+          },
+          {
+            cost: ["Fire", "Colorless"],
+            name: "Ember",
+            text: "Discard I Fire Energy card attached to Charmander in order to use this attack.",
+            damage: 30
+          }
+        ],
+        description: 'Obviously prefers hot places. If it gets caught in the rain, steam is said to spout from the tip of his tail.',
+        level: 10,
+        weaknesses: ["Water"],
+        resistance: [],
+        retreatCost: ["Colorless"],
+        artist: 'Mitsuhiro Arita',
+        number: '46/102'
+      }; */
 
-    this.pinataService.uploadFileToIPFS(image).pipe(
+    this.pinataService.uploadFileToIPFS(newCard.image as File).pipe(
       switchMap(response1 => {
         console.log("Card image response:", response1);
-        card.image = response1.pinataURL;
-        return this.pinataService.uploadJSONToIPFS(card);
+        newCard.image = response1.pinataURL;
+        return this.pinataService.uploadJSONToIPFS(newCard);
       }),
       switchMap(response2 => from(this.walletService.mintNFT(response2.pinataURL)))
     ).subscribe({
