a77e908e239b7d2369ba2b8b2b9fda0df7b83239
Alberto <alberto.presenti@eng.it>
2022-12-11 16:36:01 +0100

    create component energy chooser

---
+++ src/app/shared/energy-chooser/energy-chooser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-energy-chooser',
+  templateUrl: './energy-chooser.component.html',
+  styleUrls: ['./energy-chooser.component.scss']
+})
+export class EnergyChooserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
