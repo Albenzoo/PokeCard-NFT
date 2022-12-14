bd789eadc9900567accb273afd1f2132faa1dfef
Alberto <alberto.presenti@eng.it>
2022-12-14 20:18:44 +0100

    Output event

--- src/app/shared/energy-chooser/energy-chooser.component.ts
+++ src/app/shared/energy-chooser/energy-chooser.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Energy } from 'src/app/core/models/card';
 
 @Component({
   selector: 'app-energy-chooser',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnergyChooserComponent implements OnInit {
 
+  @Input() listLabel: string = "Energy type Selected";
+  @Input() chooserLabel: string = "Select Energy Type";
+  @Input() mode: string = "multi";
+  @Output() selectedSingleEnergy = new EventEmitter<Energy>();
+  @Output() selectedMultipleEnergy = new EventEmitter<Energy>();
+  @Output() deleteEnergy = new EventEmitter<Energy>();
+
+
+  energyTypes: Energy[] = ['Fire', 'Fighting', 'Lightning', 'Grass', 'Water', 'Psychic', 'Darkness', 'Metal', 'Colorless'];
+  selectedEnergies: Energy[] = [];
+  singleEnergy: Energy = null;
   constructor() { }
 
   ngOnInit(): void {
   }
+  removeEnergy(energy: Energy): void {
+    const index = this.selectedEnergies.indexOf(energy, 0);
+    if (index > -1) {
+      this.selectedEnergies.splice(index, 1);
+      this.deleteEnergy.emit(energy);
+    }
+  }
 
+  addEnergy(energy: Energy): void {
+    if (this.mode != "multi") {
+      this.singleEnergy = energy;
+      this.selectedSingleEnergy.emit(this.singleEnergy);
+    } else {
+      if (this.selectedEnergies.length < 4) {
+        this.selectedEnergies.push(energy);
+        this.selectedMultipleEnergy.emit(energy);
+      }
+    }
+  }
 }
