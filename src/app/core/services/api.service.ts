f2c73d03a1ed889533fd43f379bbe9c6fc7ada71
Alberto <alberto.presenti@eng.it>
2022-12-17 16:29:37 +0100

    Refining popup and spinner

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { catchError, concat, concatMap, from, map, Observable, of, switchMap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Card } from '../models/card';
 import { PinataService } from './pinata.service';
+import { SnackBarService } from './snack-bar.service';
 import { WalletService } from './wallet.service';
 
 @Injectable({
@@ -11,56 +13,31 @@ import { WalletService } from './wallet.service';
 })
 export class ApiService {
 
-  constructor(private http: HttpClient, private pinataService: PinataService, public walletService: WalletService) { }
+  constructor(private snackBarService: SnackBarService, private spinner: NgxSpinnerService, private http: HttpClient, private pinataService: PinataService, public walletService: WalletService) { }
 
 
 
 
   loadCardToIPFS(newCard: Card) {
-    /*   let card: Card = {
-        name: "Charmander",
-        hp: 50,
-        image: image,
-        length: 2.0,
-        weight: 19,
-        type: 'Lizard',
-        energy_type: 'Fire',
-        rarity: 'Common',
-        attack_list: [
-          {
-            cost: ["Colorless"],
-            name: "Scratch",
-            damage: 10
-          },
-          {
-            cost: ["Fire", "Colorless"],
-            name: "Ember",
-            text: "Discard I Fire Energy card attached to Charmander in order to use this attack.",
-            damage: 30
-          }
-        ],
-        description: 'Obviously prefers hot places. If it gets caught in the rain, steam is said to spout from the tip of his tail.',
-        level: 10,
-        weaknesses: ["Water"],
-        resistance: [],
-        retreatCost: ["Colorless"],
-        artist: 'Mitsuhiro Arita',
-        number: '46/102'
-      }; */
-
+    this.spinner.show();
     this.pinataService.uploadFileToIPFS(newCard.image as File).pipe(
       switchMap(response1 => {
         console.log("Card image response:", response1);
         newCard.image = response1.pinataURL;
         return this.pinataService.uploadJSONToIPFS(newCard);
       }),
-      switchMap(response2 => from(this.walletService.mintNFT(response2.pinataURL)))
+      switchMap(response2 => of(this.walletService.mintNFT(response2.pinataURL)))
     ).subscribe({
       next: (data: any) => {
-        console.log("Finale response:", data);
+        console.log("next");
       },
       error: (error: any) => {
+        this.snackBarService.openSnackBar(error, "OK", true);
+        this.spinner.hide();
         console.log("error uploading the card:", error);
+      },
+      complete: () => {
+        console.log("Completed");
       },
     });
 
--- src/app/core/services/snack-bar.service.ts
+++ src/app/core/services/snack-bar.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackBarService {
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
 
   constructor(private snackBar: MatSnackBar) { }
 
-  openSnackBar(message: string, action: string) {
+  openSnackBar(message: string, action: string, isError?: boolean) {
     this.snackBar.open(message, action, {
-      duration: 2000,
+      duration: 4000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      panelClass: isError ? ['red-snackbar'] : []
     });
   }
 }
