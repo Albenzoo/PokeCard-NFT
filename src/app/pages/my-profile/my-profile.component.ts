c42bf4daadcaef428e2546f66829321fb36fb175
Alberto <alberto.presenti@eng.it>
2022-12-17 16:29:51 +0100

    created my-profile component

---
+++ src/app/pages/my-profile/my-profile.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './my-profile.component.html',
+  styleUrls: ['./my-profile.component.scss']
+})
+export class MyProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
