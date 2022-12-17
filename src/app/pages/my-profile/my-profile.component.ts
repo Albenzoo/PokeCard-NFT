954ee9404ab38f8a0cbee24dc154c30fe313792f
Alberto <alberto.presenti@eng.it>
2022-12-17 23:43:15 +0100

    create my-profile component

--- src/app/pages/my-profile/my-profile.component.ts
+++ src/app/pages/my-profile/my-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { WalletService } from 'src/app/core/services/wallet.service';
 
 @Component({
   templateUrl: './my-profile.component.html',
@@ -6,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private spinner: NgxSpinnerService, public wallet: WalletService) { }
 
   ngOnInit(): void {
+    if (this.wallet.myNfts.length == 0) {
+      this.getMyNfts();
+    } else {
+      // Subscribe to the isConnected Observable
+      this.wallet.isConnected$.subscribe(isConnected => {
+        if (isConnected === true && this.wallet.myNfts.length == 0) {
+          this.getMyNfts();
+        }
+      })
+    }
   }
+  getMyNfts() {
+    this.spinner.show();
+    this.wallet.getMyNFTs();
+  }
+
 
 }
