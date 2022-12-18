f51cdb652440a6a9974c3bbf90a62997bef78e27
Alberto <alberto.presenti@eng.it>
2022-12-18 21:51:31 +0100

    add all cards page

---
+++ src/app/pages/all-cards/all-cards.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Card } from 'src/app/core/models/card';
+import { WalletService } from 'src/app/core/services/wallet.service';
+
+@Component({
+  templateUrl: './all-cards.component.html',
+  styleUrls: ['./all-cards.component.scss']
+})
+export class AllCardsComponent implements OnInit {
+
+  constructor(private router: Router, public wallet: WalletService, private spinner: NgxSpinnerService,) { }
+
+  ngOnInit(): void {
+    if (this.wallet.allNfts.length == 0 || this.wallet.allNfts.length <= 5) {
+      this.spinner.show();
+      this.wallet.getAllNFTs();
+    }
+  }
+
+  goToCardDetail(cardClicked: Card) {
+    this.wallet.cardDetail = cardClicked;
+    this.router.navigate([`/card-detail`]);
+  }
+
+}
