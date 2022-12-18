89991281affac90232454c342dcd472fb6db4530
Alberto <alberto.presenti@eng.it>
2022-12-19 00:31:12 +0100

    show correctly last nfts

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { Card } from 'src/app/core/models/card';
 import { ApiService } from 'src/app/core/services/api.service';
 import { SnackBarService } from 'src/app/core/services/snack-bar.service';
 import { WalletService } from 'src/app/core/services/wallet.service';
@@ -11,13 +13,17 @@ import { WalletService } from 'src/app/core/services/wallet.service';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor(public wallet: WalletService, public apiService: ApiService, private spinner: NgxSpinnerService, private snackBarService: SnackBarService) { }
+  constructor(private router: Router, public wallet: WalletService, public apiService: ApiService, private spinner: NgxSpinnerService, private snackBarService: SnackBarService) { }
 
   ngOnInit(): void {
-    if (this.wallet.allNfts.length == 0) {
-      this.spinner.show();
-      this.wallet.getFirstsNFTs();
-    }
+    this.spinner.show();
+    this.wallet.lastNfts = [];
+    this.wallet.getLastNFTs();
+  }
+
+  goToCardDetail(cardClicked: Card) {
+    this.wallet.cardDetail = cardClicked;
+    this.router.navigate([`/card-detail`]);
   }
 
 }
