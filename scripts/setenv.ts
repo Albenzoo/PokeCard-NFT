5d249ac5edece6f7f8cb2ae71bff059781ac5384
Alberto <alberto.presenti@eng.it>
2022-12-28 22:51:48 +0100

    còean environment

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -13,21 +13,11 @@ const targetPath = isProduction
 const environmentFileContent = `
 export const environment = {
    production: ${isProduction},
-   contractAddress: '0x682A0cD9B74cEaB00417B6391fb96B6a00b46C1B',
-   PUBLIC_KEY: '0x6E9404FAdB9B092DeBDb1d883d98B9C5A4FfBa37',
-   apiUrl: 'https://eth-goerli.g.alchemy.com/v2/201UMLnLvF_3ShqOfxSOX_-jiXKbRe7H',
-   ipfsBaseUrl: 'https://ipfs.io/',
-   ipfsBaseUrlGeteway: [
-    'https://ipfs.io/',
-    'https://hardbin.com/',
-    'https://cloudflare-ipfs.com/',
-    'https://gateway.ipfs.io/',
-    'https://gateway.pinata.cloud/'
-    ],
-    PINATA_API_KEY: "${process.env["PINATA_API_KEY"]}",
-    PINATA_API_SECRET: "${process.env["PINATA_API_SECRET"]}",
-    PINATA_JWT: "${process.env["PINATA_JWT"]}",
-    pinataBaseUrl: "https://api.pinata.cloud/",
+   contractAddress: "${process.env["CONTRACT_ADDRESS"]}",
+   PUBLIC_KEY: "${process.env["PUBLIC_KEY"]}",
+   ipfsBaseUrl: "${process.env["ipfsBaseUrl"]}",
+   PINATA_JWT: "${process.env["PINATA_JWT"]}",
+   pinataBaseUrl: "${process.env["pinataBaseUrl"]}",
 };
     `;
 // write the content to the respective file
