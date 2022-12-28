1f18e173510b0d749dc9d675812653ffa05c9160
Alberto <alberto.presenti@eng.it>
2022-12-28 16:56:56 +0100

    update project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/project-web3'),
+      dir: require('path').join(__dirname, './coverage/pokecard-nft'),
       subdir: '.',
       reporters: [
         { type: 'html' },
